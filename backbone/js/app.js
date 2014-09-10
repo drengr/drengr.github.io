5cefc95f91963ffff64a39ee71886fde9622fe76
Drengr <drengr88@gmail.com>
2014-09-10 23:38:40 +0300

    no message

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,13 +1,18 @@
 
    function getData() {
-	   var x = $.getJSON("https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc").success(console.log(result));
+	   var x = $.getJSON("https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc")
+	   .success(
+		   function (result){
+		   console.log(result);
+		   }
+	   );
 	   console.log(x);
 	}
 	
 
-	$.ajax({
+	/*$.ajax({
 		url: 'https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc',
 		dataType: 'jsonp',
 		success: console.log('Пришли данные:' +data),
 		error:  console.log('Error')
-	});
+	});*/
