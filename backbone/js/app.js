dafb96d5187ca44760f5b0a0475889ceab5f19df
drengr <drengr88@gmail.com>
2014-09-10 23:43:56 +0300

    Update app.js

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,6 +1,6 @@
 
    function getData() {
-	   $.getJSON("https://api.instagram.com/v1/tags/snow/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc")
+	   var res = $.getJSON("https://api.instagram.com/v1/tags/snow/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc")
 	   .success(
 		   function (result){
 		   console.log(result);
