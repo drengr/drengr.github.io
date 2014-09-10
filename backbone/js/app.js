cab72f76d33ae29b11efd22eec8843e26fc5575d
Drengr <drengr88@gmail.com>
2014-09-11 01:29:19 +0300

    no message

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -3,21 +3,22 @@ var config = {
 clientID: 'dee9a7204df14a7ca686a94a9aaaa320',
 apiHost: 'https://api.instagram.com/'
 }
-   function getData() {
-	   $.getJSON(config.apiHost + '/v1/tags/dogs/media/recent?callback=?&client_id=' + config.clientID)
+
+	  /* $.getJSON(config.apiHost + '/v1/tags/dogs/media/recent?callback=?&client_id=' + config.clientID)
 	   .success(
 		   function (result){
 		   console.log('Server answer: ' +result);
 		   }
 	   )
 	   .error(function(){console.log('ERROR')});
-	   console.log(y);
-	}
+	   console.log(y);*/
 	
-
-	/*$.ajax({
-		url: 'https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc',
+   function getData() {
+	$.ajax({
 		dataType: 'jsonp',
-		success: console.log('Пришли данные:' +data),
+		cache: false,
+		url: config.apiHost + '/v1/tags/dogs/media/recent?callback=?&client_id=' + config.clientID,
+		success: function(result) {console.log(result)},
 		error:  console.log('Error')
-	});*/
+	})
+   }
