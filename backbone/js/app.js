a3de8dfe2b7c7e9c52df80e9db7c5a07d576cce5
Drengr <drengr88@gmail.com>
2014-09-10 23:56:50 +0300

    no message

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,6 +1,6 @@
 
    function getData() {
-	   var y = $.getJSON("https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc")
+	   var y = $.getJSON("https://api.instagram.com/v1/users/1574083/?access_token=e176f05b56f940658a91d14141e3d1dc")
 	   .success(
 		   function (result){
 		   console.log(result);
