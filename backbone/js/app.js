a3c9204c7ca913992770e6a3bafc8cd41af316ba
Drengr <drengr88@gmail.com>
2014-09-10 22:53:41 +0300

    app.js start work with api

---
+++ backbone/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $.getJSON("https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc",
+        function(data){
+         /* $.each(data.items, function(i,item){
+            $("<img/>").attr("src", item.media.m).appendTo("#images");
+            if ( i == 3 ) return false;
+          });*/
+		console.log('ok');
+        });
+  });
