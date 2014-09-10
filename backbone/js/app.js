fdbacdb48a58c02e846a4c925c13c62395119b40
Drengr <drengr88@gmail.com>
2014-09-10 23:20:44 +0300

    no message

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -12,7 +12,6 @@
 function getData() {
 	$.ajax({
 		url: 'https://api.instagram.com/v1/tags/football/media/recent?access_token=e176f05b56f940658a91d14141e3d1dc',
-		dataType: 'jsonp',
 		success: console.log('Пришли данные:' +data),
 		error:  console.log('Error')
 	});
