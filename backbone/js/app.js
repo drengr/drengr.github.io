bbe582694a9d25dccafc4327dce3b12e8e14c84c
Drengr <drengr88@gmail.com>
2014-09-11 00:58:56 +0300

    no message

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -1,6 +1,10 @@
-
+var config = {
+apiKey: 'e176f05b56f940658a91d14141e3d1dc',
+clientID: 'b52af783c2bd45729d6263a0bed7af08',
+apiHost: 'https://api.instagram.com/'
+}
    function getData() {
-	   var y = $.getJSON("https://api.instagram.com/v1/users/1574083/?access_token=e176f05b56f940658a91d14141e3d1dc")
+	   var y = $.getJSON(config.apiHost + '/v1/tags/dogs/media/recent?callback=?&client_id=' + config.clientID)
 	   .success(
 		   function (result){
 		   console.log(result);
